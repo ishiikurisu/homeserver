994c607cbc403758d29bf8699f2a42d2223564fd
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-01 15:38:37 -0200

    Updating .gitignore

--- bot/bot.go
+++ bot/bot.go
@@ -1,13 +1,69 @@
 package bot
 
+import (
+    "gopkg.in/yaml.v2"
+    "io/ioutil"
+    "strconv"
+    "fmt"
+)
+
 type Bot struct {
-    Api string
+    Token string
+    Allowed []int
 
 }
 
+// Creates an empty bot.
 func Empty() *Bot {
     bot := Bot {
-        Api: "",
+        Token: "",
+        Allowed: make([]int, 0),
     }
     return &bot
+}
+
+// Creates a new bot. It requires the bot's Telegram token and the path to the
+// allowed ids list. If the path is empty, it will try to load from
+// `./data/homeserver/allowed.yml`.
+func New(token, allowedPath string) (*Bot, error) {
+    bot := Empty()
+    allowed := make([]int, 0)
+    path := allowedPath
+
+    if len(path) == 0 {
+        path = "./data/homeserver/allowed.yml"
+    }
+    allowed, oops := LoadAllowed(path)
+
+    bot.Token = token
+    bot.Allowed = allowed
+    return bot, oops
+}
+
+// This file must be a YAML file with a `allowed` list where each item must be
+// an allowed id to use the bot.
+func LoadAllowed(where string) ([]int, error) {
+    outlet := make([]int, 0)
+    raw, oops := ioutil.ReadFile(where)
+    if oops != nil {
+        return outlet, oops
+    }
+
+    var f interface{}
+    oops = yaml.Unmarshal(raw, &f)
+    if oops != nil {
+        return outlet, oops
+    }
+
+    everything := f.(map[interface{}]interface{})
+    midlet := everything["allowed"].([]interface{})
+    for _, it := range midlet {
+        v, oops := strconv.Atoi(fmt.Sprintf("%v", it))
+        if oops != nil {
+            return make([]int, 0), oops
+        }
+        outlet = append(outlet, v)
+    }
+
+    return outlet, nil
 }
--- bot/bot_test.go
+++ bot/bot_test.go
@@ -2,12 +2,32 @@ package bot
 
 import (
     "testing"
+    "fmt"
 )
 
 func TestCanCreateAnEmptyBot(t *testing.T) {
     bot := Empty()
-    if len(bot.Api) > 0 {
+    if len(bot.Token) > 0 || len(bot.Allowed) > 0 {
         t.Error("There is an API here? Why?")
         return
     }
+}
+
+func TestCanCreateANewBot(t *testing.T) {
+    bot, oops := New("0", "")
+    if oops != nil {
+        t.Error(fmt.Sprintf("%s\n", oops))
+        return
+    }
+    fmt.Printf("%s\n", bot.Token)
+}
+
+func TestLoadingConfigurationFile(t *testing.T) {
+    allowed, oops := LoadAllowed("./data/homeserver/allowed.yml")
+    if len(allowed) != 1 {
+        t.Error("Couldn't load the allowed ids' file")
+    } else if oops != nil {
+        t.Error(fmt.Sprintf("Some other error happenned while trying to load the allowed ids file: %s\n", oops))
+    }
+    // IDEA Check for corruption in data
 }
