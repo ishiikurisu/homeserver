1f5a6764b17e70b36f9564ca87dafc6aefcd745c
Cristiano Alves da Silva Jr <cristianoalvesjr@gmail.com>
2017-12-06 10:17:32 -0200

    Started site design

--- server/view.go
+++ server/view.go
@@ -26,9 +26,9 @@ func LoadFile(path string) (string, error) {
   return outlet, oops
 }
 
-func (view *View) LoadIndex() string {
+func (view *View) LoadPage(page string) string {
   buffer := bytes.NewBufferString("")
-  rawHtml, _ := LoadFile(view.Pwd + "assets/index.html")
+  rawHtml, _ := LoadFile(view.Pwd + page)
   rawCss, _ := LoadFile(view.Pwd + "assets/css/master.css")
   rawJS, _ := LoadFile(view.Pwd + "assets/js/app.js")
 
@@ -40,13 +40,14 @@ func (view *View) LoadIndex() string {
     Script: templ.JS(rawJS),
   }
 
-  template, oops := templ.New("index").Parse(rawHtml)
-  if oops != nil {
-    panic(oops)
-  }
+  template, _ := templ.New("index").Parse(rawHtml)
   template.Execute(buffer, custom)
   outlet := buffer.String()
 
   return outlet
+}
+
+func (view *View) LoadIndex() string {
+  return view.LoadPage("assets/index.html")
 }
 
