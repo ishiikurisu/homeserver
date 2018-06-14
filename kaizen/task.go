21acc3a412cbfa2d40d45411f113b96f7ea53af2
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-06-14 14:51:34 -0300

    Failing at executing some tasks

--- kaizen/task.go
+++ kaizen/task.go
@@ -1,5 +1,13 @@
 package kaizen
 
+import (
+    "github.com/Shopify/go-lua"
+    "os"
+    "path/filepath"
+    "math/rand"
+    "encoding/hex"
+)
+
 type Task struct {
     Script string
 }
@@ -13,5 +21,31 @@ func NewTaskFromScript(script string) *Task {
 }
 
 func (task *Task) Run() error {
+    tempSrc := TempFileName(".", ".lua")
+    if fp, oops := os.Create(tempSrc); oops != nil {
+        return oops
+    } else {
+        fp.WriteString(task.Script)
+        fp.Close()
+    }
+
+    l := lua.NewState()
+    lua.OpenLibraries(l)
+    if oops := lua.DoFile(l, tempSrc); oops != nil {
+        return oops
+    }
+
+    if oops := os.Remove(tempSrc); oops != nil {
+        return oops
+    }
+
     return nil
+}
+
+// Generates a temporary filename for use in testing or whatever.
+// From https://stackoverflow.com/questions/28005865/golang-generate-unique-filename-with-extension
+func TempFileName(prefix, suffix string) string {
+    randBytes := make([]byte, 16)
+    rand.Read(randBytes)
+    return filepath.Join(os.TempDir(), prefix+hex.EncodeToString(randBytes) + suffix)
 }
--- kaizen/task_test.go
+++ kaizen/task_test.go
@@ -2,6 +2,7 @@ package kaizen
 
 import (
     "testing"
+    "fmt"
 )
 
 
@@ -10,6 +11,35 @@ func TestEmptyTaskCanBeExecuted(t *testing.T) {
     task := NewTaskFromScript(emptyScript)
     oops := task.Run()
     if oops != nil {
-        t.Error("Task could not be run")
+        t.Error("Empty task could not be run")
+    }
+}
+
+func TestTaskCanBeExecuted(t *testing.T) {
+    textScript := `
+    x = 12
+    y = 10
+    print(x+y)
+    `
+    task := NewTaskFromScript(textScript)
+    oops := task.Run()
+    if oops != nil {
+        t.Error("Task from text could not be run")
+    }
+}
+
+func TestWrongTaskShouldNotBeExecuted(t *testing.T) {
+    wrongScript := `
+    function f(x)
+        return x*x
+    end
+    print(g(10))
+    `
+    task := NewTaskFromScript(wrongScript)
+    oops := task.Run()
+    if oops == nil {
+        t.Error("Wrong task could be run")
+    } else {
+        fmt.Printf("%v", oops)
     }
 }
